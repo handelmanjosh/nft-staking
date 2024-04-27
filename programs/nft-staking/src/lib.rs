e5f2e13f6c6af301b931a03e50ba6d2ad94b5a7b
josh handelman <handelmanjos@gmail.com>
2024-04-26 21:16:12 -0400

    stuff

--- programs/nft-staking/src/lib.rs
+++ programs/nft-staking/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{mint_to, MintTo, Mint, TokenAccount, Token};
+use anchor_spl::token::{mint_to, MintTo, Mint, TokenAccount, Token, transfer, Transfer};
 use anchor_spl::associated_token::AssociatedToken;
 declare_id!("E51uGYbVrPyLffi5qVgjxvYaaRA1StrJjqq3J7HnSpCV");
 
@@ -7,7 +7,7 @@ declare_id!("E51uGYbVrPyLffi5qVgjxvYaaRA1StrJjqq3J7HnSpCV");
 pub mod nft_staking {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         msg!("Token state initialized");
         Ok(())
     }
@@ -15,6 +15,22 @@ pub mod nft_staking {
     pub fn stake(ctx: Context<Stake>) -> Result<()> {
         // make pda for nft collection
         // transfer nft to pda
+        transfer(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                Transfer {
+                    from: ctx.accounts.nft_account.to_account_info(),
+                    to: ctx.accounts.stake_account.to_account_info(),
+                    authority: ctx.accounts.user.to_account_info(),
+                }
+            ),
+            1
+        )?; // remember to add error handling
+        let staking_account = &mut ctx.accounts.stake_account;
+        staking_account.staked_time = Clock::get()?.unix_timestamp;
+        staking_account.owner = ctx.accounts.user.key();
+        staking_account.mint = ctx.accounts.nft_account.mint;
+        msg!("Nft staked: {}", ctx.accounts.nft_account.mint.to_string());
         Ok(())
     }
     pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
@@ -39,12 +55,35 @@ pub struct Initialize<'info> {
     pub user: Signer<'info>,
     pub token_program: Program<'info, Token>,
     pub rent: Sysvar<'info, Rent>,
-    pub system_program: Program<'info, System>
+    pub system_program: Program<'info, System>,
 }
 
+#[account]
+pub struct StakeInfo {
+    owner: Pubkey,
+    mint: Pubkey,
+    staked_time: i64,
+}
 #[derive(Accounts)]
-pub struct Stake {
-
+pub struct Stake<'info> {
+    #[account(
+        init,
+        seeds = [b"stake", user.key().as_ref(), nft_account.key().as_ref()],
+        bump,
+        payer = user,
+        space = 32 + 32 + 8,
+    )]
+    pub stake_account: Account<'info, StakeInfo>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        constraint = nft_account.owner == user.key(),
+        constraint = nft_account.amount == 1,
+    )]
+    pub nft_account: Account<'info, TokenAccount>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
 }
 
 #[derive(Accounts)]
