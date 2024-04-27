bd49a83b9474a0d9dfaf46ae4037c7a10d989c50
handelmanjosh <handelmanjos@gmail.com>
2024-04-26 21:15:15 -0400

    stuff

--- tests/nft-staking.ts
+++ tests/nft-staking.ts
@@ -7,10 +7,21 @@ describe("nft-staking", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.NftStaking as Program<NftStaking>;
-
+  const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("mint")],
+    program.programId,
+  );
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initialize().accounts({
+      mint,
+    }).rpc();
     console.log("Your transaction signature", tx);
+  });
+  it("can stake nft", async () => {
+
+  });
+  it("can unstake nft, getting tokens", async () => {
+
   });
 });
